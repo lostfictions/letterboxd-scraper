31186ea7c99313557dd88376b8c75f49718c3a0d
sa <sixsecondfiesta@gmail.com>
2016-07-02 19:57:12 -0400

    little cleanup

--- scrape.js
+++ scrape.js
@@ -1,44 +1,70 @@
-/*eslint semi: ["error", "never"], strict: 0*/
+#!/usr/bin/env node
+/*eslint strict: 0*/
 'use strict'
-
-const uri = 'https://letterboxd.com/bpfvidnite/watchlist/'
 
 const url = require('url')
 const Nightmare = require('nightmare')
 const vo = require('vo')
+const Progress = require('progress')
+
+const argv = require('minimist')(process.argv.slice(2))
+
+const uri = argv._.length > 0 ?
+  argv._[0] :
+  'https://letterboxd.com/bpfvidnite/watchlist/'
 
 vo(run)(function(err, result) {
   if(err) {
     throw err
   }
-  console.log(result.length)
-  // console.dir(result.map(u => url.resolve(uri, u)))
+  console.log(`Got ${result.length} links.`)
   require('fs').writeFileSync('links.txt', result.map(u => url.resolve(uri, u)).join('\n'))
   console.log('done.')
 })
 
 function* run() {
   const scraper = Nightmare()
-  const links = []
 
+  scraper.on('page', function(type, message, stack) {
+    console.error(`On page: [${type}] "${message}"
+    Stack:
+    ${stack}`)
+  })
+  scraper.on('console', function(type, ...args) {
+    console.log(`Console on page [${type}]: "${args.join('/')}"`)
+  })
+  
   yield scraper.goto(uri)
+  
+  const pages = yield scraper.evaluate(() => Number.parseInt(document.querySelector('.paginate-pages li:last-child a').textContent, 10))
+
+  const progress = new Progress(':current/:total [:bar] :eta s', { total: pages })
+
+  const links = []
 
   links.push(...yield scraper.evaluate(() =>
     Array
       .from(document.querySelectorAll('.linked-film-poster a'))
       .map(n => n.getAttribute('href'))
   ))
+  progress.tick()
   
-  while(yield scraper.visible('.paginate-next')) {
-    yield scraper.click('.paginate-next')
+  for(let page = 2; page <= pages; page++) {
+    yield scraper
+      .goto('https://letterboxd.com/bpfvidnite/watchlist/page/' + page + '/')
+      .wait('footer')
 
-    links.push(...yield scraper.evaluate(() =>
+    const ls = yield scraper.evaluate(() =>
       Array
         .from(document.querySelectorAll('.linked-film-poster a'))
         .map(n => n.getAttribute('href'))
-    ))
-  }
+    )
 
+    links.push(...ls)
+
+    progress.tick()
+  }
+  
   yield scraper.end()
 
   return links 
